383f0bd26b296f9fb02901c7b6ccdb5445c8ea30
Bananeq <dankilom26@gmail.com>
2023-11-17 18:11:14 +0100

    hw02: task reverse-tree

---
+++ hw02/reverse-tree.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (reverse-tree bst)
+  (if (null? bst)
+      '()
+      (let* ([root (car bst)]
+             [left (cadr bst)]
+             [right (caddr bst)]
+             [reversed-left (reverse-tree left)]
+             [reversed-right (reverse-tree right)])
+        (cons root (cons reversed-right (cons reversed-left '()))))))
+(define (test)
+  (define (check-equal? x y)
+    (if (equal? x y)
+        (display "Test passed\n")
+        (display "Test failed\n")))
+  (check-equal? (reverse-tree '()) '())
+  (check-equal? (reverse-tree '(1 () ())) '(1 () ()))
+  (check-equal? (reverse-tree '(1 (2 () ()) ())) '(1 () (2 () ())))
+  (check-equal? (reverse-tree '(2 (1 () ()) (3 () ()))) '(2 (3 () ()) (1 () ())))
+  (check-equal? (reverse-tree '(10 (5 (3 () ()) ()) (20 () (25 () ())))) '(10 (20 (25 () ()) ()) (5 () (3 () ())))))
+(test)
