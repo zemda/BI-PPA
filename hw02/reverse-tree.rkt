260d82a4853ce2e0d9894a55d3e21882e2870707
Bananeq <dankilom26@gmail.com>
2023-11-17 18:16:27 +0100

    hw02: refactor reverse-tree

--- hw02/reverse-tree.rkt
+++ hw02/reverse-tree.rkt
@@ -1,4 +1,6 @@
 #lang racket
+
+
 (define (reverse-tree bst)
   (if (null? bst)
       '()
@@ -8,6 +10,8 @@
              [reversed-left (reverse-tree left)]
              [reversed-right (reverse-tree right)])
         (cons root (cons reversed-right (cons reversed-left '()))))))
+
+
 (define (test)
   (define (check-equal? x y)
     (if (equal? x y)
@@ -18,4 +22,5 @@
   (check-equal? (reverse-tree '(1 (2 () ()) ())) '(1 () (2 () ())))
   (check-equal? (reverse-tree '(2 (1 () ()) (3 () ()))) '(2 (3 () ()) (1 () ())))
   (check-equal? (reverse-tree '(10 (5 (3 () ()) ()) (20 () (25 () ())))) '(10 (20 (25 () ()) ()) (5 () (3 () ())))))
+
 (test)
